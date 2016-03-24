aaf030dfc1ce74fe0aa3580c4d20647b2231df57
candyj0311 <cij2104@tc.columbia.edu>
2016-03-23 20:41:04 -0400

    Log In and Sign Up

--- app/src/main/java/candacelizelle/finalproject_candacejlizelles/MainActivity.java
+++ app/src/main/java/candacelizelle/finalproject_candacejlizelles/MainActivity.java
@@ -1,14 +1,105 @@
 package candacelizelle.finalproject_candacejlizelles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private Firebase rootRef;
+    private Firebase userRef;
+    private Firebase.AuthStateListener authStateListener;
 
-    // Hello from Lizelle's laptop!
+    private EditText keyEditText;
+    private EditText valueEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        keyEditText = (EditText) findViewById(R.id.edit_text_key);
+        valueEditText = (EditText) findViewById(R.id.edit_text_value);
+
+        Firebase.setAndroidContext(this);
+        rootRef = new Firebase("https://finalprojectcjls.firebaseio.com");
+
+        authStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                if (authData != null) {
+                    userRef = rootRef.child("users/" + authData.getUid());
+                } else {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    public void writeToCloud(View view) {
+        if (userRef != null)
+            userRef.child(keyEditText.getText().toString()).setValue(valueEditText.getText().toString());
+    }
+
+    public void readFromCloud(View view) {
+        if (userRef == null) return;
+        userRef.child(keyEditText.getText().toString()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.getValue() != null) {
+                    valueEditText.setText(snapshot.getValue().toString());
+                } else {
+                    valueEditText.setText("");
+                    Toast.makeText(MainActivity.this, "Nope: " + keyEditText.getText().toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+                Toast.makeText(MainActivity.this, "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        rootRef.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        rootRef.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log_out:
+                rootRef.unauth();
+                keyEditText.setText("");
+                valueEditText.setText("");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
