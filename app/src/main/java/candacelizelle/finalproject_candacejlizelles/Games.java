8a429a4481fbc944173596bfe83262445b082e2d
candyj0311 <cij2104@tc.columbia.edu>
2016-03-31 18:30:21 -0400

    Clarified a comment

--- app/src/main/java/candacelizelle/finalproject_candacejlizelles/Games.java
+++ app/src/main/java/candacelizelle/finalproject_candacejlizelles/Games.java
@@ -9,7 +9,7 @@ public class Games extends AppCompatActivity {
     private String categoryName;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_screen1);
 
@@ -17,10 +17,9 @@ public class Games extends AppCompatActivity {
         categoryName = getIntent().getStringExtra(BundleKey.NAME_KEY);
         setTitle(categoryName);
 
-// Why is this so complex?
-//        if (categoryName.equals("Fruit")) {}
-    }
+// Get game content - which way do we want to do this? Xi says not to use Firebase
 
+    }
 
     public void showResults(View view) {
         Intent intent = new Intent(Games.this, Results.class);
--- app/src/main/java/candacelizelle/finalproject_candacejlizelles/Results.java
+++ app/src/main/java/candacelizelle/finalproject_candacejlizelles/Results.java
@@ -1,7 +1,10 @@
 package candacelizelle.finalproject_candacejlizelles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class Results extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class Results extends AppCompatActivity {
         setContentView(R.layout.results);
     }
 
+    public void playAgain(View view) {
+        Toast.makeText(Results.this, "How do I do this?", Toast.LENGTH_SHORT).show();
+    }
 
+    public void chooseAnotherGame(View view) {
+        Intent intent = new Intent(Results.this, CategoriesPage.class);
+        startActivity(intent);
+    }
 }
