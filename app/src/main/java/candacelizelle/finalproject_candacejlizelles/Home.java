b5bd50a6c6ec66a6dae16b6443ab18038018975b
candyj0311 <cij2104@tc.columbia.edu>
2016-04-02 12:17:12 -0400

    Cleaned up code for Results layout

--- app/src/main/java/candacelizelle/finalproject_candacejlizelles/Home.java
+++ app/src/main/java/candacelizelle/finalproject_candacejlizelles/Home.java
@@ -1,8 +1,8 @@
 package candacelizelle.finalproject_candacejlizelles;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
